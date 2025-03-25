dcbcc4ce45cf17307b9caff15f5fa2699f9abf7d
LucasBb07 <barcelloslucas07@gmail.com>
2025-03-25 17:53:51 -0300

    aula3

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  <View style={styles.coluna}>
+      <TouchableOpacity style={styles.view1}>
+       <Image source={require('./assets/favicon.png')} />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view2}>
+      <Image source={require('./assets/favicon.png')} />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view3}>
+      <Image source={require('./assets/favicon.png')} />
+      </TouchableOpacity>
+    
     </View>
+
+    <View style={styles.coluna}>
+      <TouchableOpacity style={styles.view1}>
+      <Image source={require('./assets/favicon.png')} />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view2}>
+      <Image source={require('./assets/favicon.png')} />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.view3}>
+      <Image source={require('./assets/favicon.png')} />
+      </TouchableOpacity>
+
+    </View>
+
+
+
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#fff', 
+    alignItems: 'center', //alinhamento horizontal
+    justifyContent: 'center',  //alinhamento vertical
+    padding: 20,
+    columnGap: 20
   },
+  view1: {
+    //flex:1,
+    backgroundColor:"#7bd415",
+    width:'100%',
+    height: 200,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+   
+  },
+  view2: {
+    //flex:1,
+    backgroundColor:"#12537a",
+    width:'100%',
+    height: 200,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  view3: {
+    //flex:1,
+    backgroundColor:"#f20a0a",
+    width:'100%',
+    height: 200,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  coluna: {
+    flexDirection: "column",
+    width: "50%",
+    rowGap: 20
+  }
 });
